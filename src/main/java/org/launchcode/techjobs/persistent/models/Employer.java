da1b04ab4fb751a84dee40307d92652814478a72
mrjermarco <107326902+mrjermarco@users.noreply.github.com>
2023-04-03 01:22:31 -0500

    Added basic setup for Employer and Skill models

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,24 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+        @NotBlank(message = "Location is required")
+        @Size(min = 5, max = 24, message = "Location must be between 5-24 characters long")
+        private String location;
+
+        public String getLocation() {
+            return location;
+        }
+
+    public static void main(String[] args) {
+
+
+    }
 
 
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,22 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
+
+    @NotBlank(message = "Description is required")
+    @Size(min = 100, max = 500, message = "Description must be between 100-500 characters long")
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public static void main(String[] args) {
+
+    }
 
 }
